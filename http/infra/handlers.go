3cf06578bf212fa4df06aa0ee34858e1fcb99dbd
Chad Grant <chad.grant@me.com>
2019-11-25 21:27:29 -0800

    adding gorliia

--- http/infra/handlers.go
+++ http/infra/handlers.go
@@ -1,6 +1,24 @@
 package infra
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func HandleGorilla(r *mux.Router) {
+	r.HandleFunc("/health/gtg", goodToGo)
+	r.HandleFunc("/health/asg", serviceCanary)
+	r.HandleFunc("/health", health)
+	r.HandleFunc("/metadata", metadata)
+	r.HandleFunc("/debug/environment", debugEnvironmentName)
+	r.HandleFunc("/debug/headers", debugHeaders)
+	r.HandleFunc("/debug/time", debugTime)
+	r.HandleFunc("/debug/error", debugError)
+	r.HandleFunc("/debug/env", debugEnvironment)
+	r.HandleFunc("/debug/filedates", debugFileDates)
+	r.HandleFunc("/debug/name", debugName)
+}
 
 func Handle() {
 	http.HandleFunc("/health/gtg", goodToGo)
