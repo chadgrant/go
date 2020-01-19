b2b844f161216008bc0808c1b3fd72feedb79b80
Chad Grant <chad.grant@me.com>
2020-01-18 18:16:21 -0800

    health checks

--- gorilla/handlers.go
+++ gorilla/handlers.go
@@ -2,13 +2,14 @@ package gorilla
 
 import (
 	"github.com/chadgrant/go-http-infra/infra"
+	"github.com/chadgrant/go-http-infra/infra/health"
 	"github.com/gorilla/mux"
 )
 
-func Handle(r *mux.Router) {
-	// r.HandleFunc("/health/liveness", infra.Liveness)
-	// r.HandleFunc("/health/readiness", infra.Readiness)
-	// r.HandleFunc("/health", infra.Health)
+func Handle(r *mux.Router, hc health.Handler) {
+	r.HandleFunc("/health/live", hc.Live)
+	r.HandleFunc("/health/ready", hc.Ready)
+	r.HandleFunc("/health", hc.Report)
 	r.HandleFunc("/metadata", infra.Metadata)
 	r.HandleFunc("/debug/environment", infra.DebugEnvironmentName)
 	r.HandleFunc("/debug/headers", infra.DebugHeaders)
